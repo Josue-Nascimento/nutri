54928a32486987530f7e2a7a4ba80758c2d7797d
Josué <rnascimentojosue@gmail.com>
2025-09-13 21:41:07 -0300

    corrigindo dominio

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/secondary/", // 👈 caminho base do site
+  base: "/secondary", // 👈 caminho base do site
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
